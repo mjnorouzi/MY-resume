58eb9b6e5b5d774344306f11c9ee7f57f9ec23b6
mjnorouzi <norouzi.mj78@gmail.com>
2019-01-19 15:34:54 +0330

    Updates

---
+++ precache-manifest.613485959394a55d9d90e126651ca7ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b3184dfab41b1a0c396a691955ae3e4",
+    "url": "/MY-resume/static/media/giphy-downsized.2b3184df.gif"
+  },
+  {
+    "revision": "a224e61a1cfbb5d28054",
+    "url": "/MY-resume/static/js/runtime~main.a224e61a.js"
+  },
+  {
+    "revision": "27dd5d7ae4b3e47d58af",
+    "url": "/MY-resume/static/js/main.27dd5d7a.chunk.js"
+  },
+  {
+    "revision": "02fb45658e6419b1872b",
+    "url": "/MY-resume/static/js/1.02fb4565.chunk.js"
+  },
+  {
+    "revision": "27dd5d7ae4b3e47d58af",
+    "url": "/MY-resume/static/css/main.43cde3d6.chunk.css"
+  },
+  {
+    "revision": "2be6b99f33dd39307cb8e57f04643db1",
+    "url": "/MY-resume/index.html"
+  }
+];
